c745e2473c568b67526cec350eb505674a664469
JeongEuiJoon <dordong327@gmail.com>
2017-10-05 17:09:58 +0900

    feed delete, feed edit

--- routes/feed.js
+++ routes/feed.js
@@ -6,9 +6,16 @@ var Feed = require('../models/feed');
 router.get('/allFeed',(req,res)=>{
     //모든 피드 검색
     Feed.find({},(err, feeds)=>{
-        if(err) res.status(404).end();
+        if(err) res.status(403).end();
         else
             res.status(200).json(feeds);
+    });
+});
+router.get('/getFeed/:standard',(req, res)=>{
+    Feed.find({district : req.params.standard},(err, feeds)=>{
+        if(err) res.status(403).end();
+        else
+            res.status(201).json(feeds);
     });
 });
 router.post('/addFeed/:userId',(req,res)=>{
@@ -30,8 +37,28 @@ router.post('/addFeed/:userId',(req,res)=>{
 
 });
 
-router.delete('/deleteFeed',(req,res)=>{
+router.delete('/deleteFeed/:feedId/:userId',(req,res)=>{
     //피드 삭제
+    //feedID로 찾고 userId와 다르면 err, 같으면 삭제
+    Feed.findOne({_id : req.params.feedId},(err,feed)=>{
+        if(err) res.status(403).end();
+        else {
+            console.log(feed);
+            if(feed.userId == req.params.userId)
+            {
+                feed.remove((err)=>{
+                    if(err) res.status(403).end();
+                    else
+                        res.status(201).send({
+                            success:true
+                        });
+                });
+            }else
+                res.status(403).send(
+                    'userId incorrect'
+                );
+        }
+    });
 });
 router.put('/editFeed/:feedId',(req,res)=>{
     //피드 수정
