852089d302387daa16ccfc27d85d208baf2f87f0
JeongEuiJoon <dordong327@gmail.com>
2017-10-31 02:18:39 +0900

    comment 등록 시간

--- bin/config.js
+++ bin/config.js
@@ -1,5 +1,5 @@
 var config_port = '5000';
-var host = "52.79.180.194:5000";
+var host = "ec2-52-79-174-252.ap-northeast-2.compute.amazonaws.com:5000";
 var imageStorage = host+"/images";
 module.exports.host = host;
 module.exports.imageStorage = imageStorage;
--- models/feed.js
+++ models/feed.js
@@ -4,22 +4,23 @@ var User = require('./user');
 var Schema = mongoose.Schema;
 
 var FeedShcema = new Schema({
-    userId : {type:String, required : true},//작성자 id
-    district : {type:String, required : true},//피드 기준=장소 (기준 추가 예정)
-    createdDate : {type :Date, default : Date.now},
-    feedBody : {type :String, default : ""},
+    userId: {type: String, required: true},//작성자 id
+    district: {type: String, required: true},//피드 기준=장소 (기준 추가 예정)
+    createdDate: {type: Date, default: Date.now},
+    feedBody: {type: String, default: ""},
 
-    likes:[String],
+    likes: [String],
 
-    comment : [{
-        userId : {type:String},//댓글 단 사람의 User._id
-        commentBody : {type:String},
-        pic_url:{type:String},
-        userName : {type:String},
-        comment_likes : {type:String,default : "0"}
+    comment: [{
+        userId: {type: String},//댓글 단 사람의 User._id
+        commentBody: {type: String},
+        pic_url: {type: String},
+        userName: {type: String},
+        comment_likes: {type: String, default: "0"},
+        createdDate: {type: Date, default: Date.now}
     }],
 
-    feed_pic_url : {type:String, default:""},
-    writer : {type:Schema.Types.ObjectId,ref : 'user'}
+    feed_pic_url: {type: String, default: ""},
+    writer: {type: Schema.Types.ObjectId, ref: 'user'}
 });
 module.exports = mongoose.model('feed', FeedShcema);
